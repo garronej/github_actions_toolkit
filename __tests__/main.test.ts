793495f0dc7b79b5fb72a3458c341799231ad23a
Joseph Garrone <joseph.garrone.gj@gmail.com>
2020-04-28 16:53:20 +0200

    update

--- __tests__/main.test.ts
+++
@@ -1,4 +0,0 @@
-
-test('throws invalid number', async () => {
-  await expect((()=>{ throw new Error("milliseconds not a number (test ran) wtf"); })()).rejects.toThrow('milliseconds not a number (test ran)')
-})
