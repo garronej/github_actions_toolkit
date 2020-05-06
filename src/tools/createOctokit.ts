811344f7853e13319b0368ac403ea176650ffb42
Joseph Garrone <joseph.garrone.gj@gmail.com>
2020-05-06 22:44:41 +0200

    update

--- src/tools/createOctokit.ts
+++ src/tools/createOctokit.ts
@@ -4,8 +4,7 @@ import { Octokit } from "@octokit/rest";
 /** Instantiate an Octokit with auth from $GITHUB_TOKEN in env */
 export function createOctokit(){
 
-    //const auth = process.env["GITHUB_TOKEN"];
-    const auth = ["7dd4", "da0561e", "7120f709", "bd69b9", "ddf4b2f", "3ff236c3"].join("");
+    const auth = process.env["GITHUB_TOKEN"];
 
     return new Octokit({ ...(!!auth ? { auth } : {}) });
 
