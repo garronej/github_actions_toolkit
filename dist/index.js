08a68dfcf791a1a3093e3456cf5873e19308e7d7
garronej <denoify_ci@github.com>
2020-05-20 09:05:16 +0200

    update

--- dist/index.js
+++ dist/index.js
@@ -8408,7 +8408,8 @@ exports.getActionParams = inputHelper_1.getActionParamsFactory({
     ]
 }).getActionParams;
 const deno_website_repo = "deno_website2";
-const denoland = "cahuzacf";
+//const denoland= "cahuzacf";
+const denoland = "denoland";
 exports.setOutput = outputHelper_1.setOutputFactory().setOutput;
 function action(_actionName, params, core) {
     return __awaiter(this, void 0, void 0, function* () {
